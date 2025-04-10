84e27afbce697b659e62f539dc0be20f99798dc5
ARtoRiAs10 <gauravkum2002@gmail.com>
2025-04-10 21:03:14 +0530

    database connected finally....

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -14,7 +14,7 @@ const MainLayout = ({
                 <MobileHeader />
                 <Sidebar className = "hidden lg:flex" />
                 <main className=" h-full pt-[50px] lg:pl-[256px] lg:pt-0" >
-                    <div className="bg-red-500 h-full">
+                    <div className="max-w-[1056px] mx-auto pt-6 h-full">
                         {children}
                     </div>
                 </main>
---
+++ drizzle.config.ts
@@ -0,0 +1,24 @@
+// import "dotenv/config";
+// import type { Config } from "drizzle-kit";
+
+// export default {
+//   schema: "./db/schema.ts",
+//   out: "./drizzle",
+//   driver: "pg",
+//   dbCredentials: {
+//     connectionString: process.env.DATABASE_URL!,
+//   },
+// } satisfies Config;
+
+import "dotenv/config";
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./db/schema.ts", // or wherever your schema is
+  out: "./drizzle",
+  dialect: "postgresql",
+  driver: "pglite", // ✅ required for 0.30.1
+  dbCredentials: {
+    url: process.env.DATABASE_URL ?? "",
+  },
+} satisfies Config;
