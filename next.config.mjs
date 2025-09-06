01a6e106c10a1e32c63f9513e06a878552040f4f
Gaurav kumar <121500265+ARtoRiAs10@users.noreply.github.com>
2025-09-07 01:32:58 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: "/api/(.*)",
-          headers: [
-            {
-              key: "Access-Control-Allow-Origin",
-              value: "*",
-            },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET, POST, PUT, DELETE, OPTIONS",
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value: "Content-Type, Authorization",
-            },
-            {
-              key: "Content-Range",
-              value: "bytes : 0-9/*",
-            },
-          ],
-        },
-      ];
-    },
-    experimental: {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          {
+            key: "Content-Range",
+            value: "bytes : 0-9/*",
+          },
+        ],
+      },
+    ];
+  },
+  experimental: {
     serverActions: {
       allowedOrigins: [
-        'localhost:3000',
-        'symmetrical-space-parakeet-44vxgxr7g54fgqw-3000.app.github.dev',
+        "localhost:3000",
+        "symmetrical-space-parakeet-44vxgxr7g54fgqw-3000.app.github.dev",
       ],
     },
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-  
-  export default nextConfig;
+
+module.exports = nextConfig;
